d76e3838c4df4335de9512376cf0683e74b447aa
FHD-Report <squidshank@gmail.com>
2026-04-22 07:05:51 +0100

    Updated compact mode, fixed field alignments, more compact export, removed disclaimer from export.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'fusic-hd-v1';
-const PRECACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'fusic-hd-v2';
+const PRECACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './FHD.jpg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)));
